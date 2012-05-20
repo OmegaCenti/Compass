5caa31148f26c2bf36ca64dd748736c0c47e39bf
aPunch <theapunch@yahoo.com>
2012-05-19 19:13:41 -0500

    Attempt to fix Compass/Communities compatibility

--- src/main/java/net/betterverse/compass/bukkit/BukkitCompass.java
+++ src/main/java/net/betterverse/compass/bukkit/BukkitCompass.java
@@ -2,6 +2,8 @@ package net.betterverse.compass.bukkit;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import net.betterverse.communities.Communities;
 import net.betterverse.compass.bukkit.hooks.MyChunksManager;
 import net.betterverse.compass.bukkit.listeners.EventListener;
 import net.betterverse.compass.bukkit.player.BukkitCompassPlayer;
@@ -15,64 +17,66 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Julian Trust
  */
 public class BukkitCompass extends JavaPlugin implements Runnable {
-    
+
     /*
-     * Standard permission required to warp to or through a block. Players with
-     * this permission invoke a PvP cooldown after teleporting.
+     * Standard permission required to warp to or through a block. Players with this permission invoke a PvP cooldown
+     * after teleporting.
      */
     public static final String warpUsePermission = "compasswarp.use";
-    
+
     /*
-     * Permission required to warp to or through a block without invoking a
-     * cooldown on PvP.
+     * Permission required to warp to or through a block without invoking a cooldown on PvP.
      */
     public static final String warpUnlimitedPermission = "compasswarp.unlimited";
-    
+
     public static BukkitCompass instance;
+    public static Communities communities;
     private Map<String, BukkitCompassPlayer> players;
     private EventListener listener;
-    
+
     @Override
     public void onEnable() {
         instance = this;
         getConfig().options().copyDefaults(true);
         saveConfig();
         players = new HashMap<String, BukkitCompassPlayer>();
-        for(Player p : getServer().getOnlinePlayers())
+        for (Player p : getServer().getOnlinePlayers())
             registerPlayer(p);
         listener = new EventListener(this);
-        if(getServer().getPluginManager().isPluginEnabled("MyChunks"))
+        if (getServer().getPluginManager().isPluginEnabled("Communities"))
+            communities = (Communities) getServer().getPluginManager().getPlugin("Communities");
+        if (getServer().getPluginManager().isPluginEnabled("MyChunks"))
             MyChunksManager.setMyChunks(getServer().getPluginManager().getPlugin("MyChunks"));
         getServer().getScheduler().scheduleSyncRepeatingTask(this, this, 0, 20);
         getServer().getLogger().info("[CompassWarp] - Enabled");
     }
-    
+
     @Override
     public void onDisable() {
         getServer().getLogger().info("[CompassWarp] - Disabled");
     }
-    
+
     public BukkitCompassPlayer getPlayer(String name) {
         return players.get(name);
     }
-    
+
     public int getCooldown() {
         return getConfig().getInt("cooldown", 30);
     }
-    
+
     @Override
     public void run() {
-        for(BukkitCompassPlayer player : players.values()) {
-            if(player.getCooldown() > 0) {
+        for (BukkitCompassPlayer player : players.values()) {
+            if (player.getCooldown() > 0) {
                 player.setCooldown(player.getCooldown() - 1);
             }
         }
     }
-    
+
     public void registerPlayer(Player player) {
         players.put(player.getName(), new BukkitCompassPlayer(player));
     }
-    
+
     public void unregisterPlayer(Player player) {
         players.remove(player.getName());
     }
--- src/main/java/net/betterverse/compass/bukkit/hooks/TownsManager.java
+++ src/main/java/net/betterverse/compass/bukkit/hooks/TownsManager.java
@@ -1,39 +1,49 @@
 package net.betterverse.compass.bukkit.hooks;
 
-import net.betterverse.communities.player.CommunityPlayer;
-import net.betterverse.communities.player.PlayerManager;
-import net.betterverse.communities.town.ChunkLocation;
-import net.betterverse.communities.town.Town;
-import net.betterverse.communities.town.TownManager;
+import net.betterverse.communities.Communities;
+import net.betterverse.communities.community.Community;
+import net.betterverse.communities.community.CommunityPlayer;
+import net.betterverse.compass.bukkit.BukkitCompass;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-/**
- * Static class for any sort of region checking required
- * of Towny (Bukkit plugin).
- *
- * @modified 1/8/12
- * @author Julian Trust
- */
 public class TownsManager {
-	
+
     public static boolean canWarpFrom(Player player) {
-				String playerTownName = PlayerManager.getCommunityPlayer(player.getName()).getCurrentTown();
-				Town chunkTown = TownManager.getChunkTown(new ChunkLocation(player.getLocation().getChunk()));
+        Communities coms = BukkitCompass.communities;
+        if (coms == null) {
+            return true;
+        }
+
+        CommunityPlayer comPlayer = coms.fromBukkitPlayer(player.getName());
+        String playerTownName = "";
+        if (comPlayer.getCommunity() != null) {
+            playerTownName = comPlayer.getCommunity().getName();
+        }
+        Community chunkTown = coms.getChunkOwner(player.getLocation().getChunk());
         String chunkTownName = "";
-				if(chunkTown != null) {
-					chunkTownName = chunkTown.getName();
-				}
-				return playerTownName.equalsIgnoreCase(chunkTownName);
+        if (chunkTown != null) {
+            chunkTownName = chunkTown.getName();
+        }
+        return playerTownName.equalsIgnoreCase(chunkTownName);
     }
-    
+
     public static boolean canWarpTo(Player player, Location location) {
-				String playerTownName = PlayerManager.getCommunityPlayer(player.getName()).getCurrentTown();
-				Town chunkTown = TownManager.getChunkTown(new ChunkLocation(location.getChunk()));
+        Communities coms = BukkitCompass.communities;
+        if (coms == null) {
+            return true;
+        }
+        CommunityPlayer comPlayer = coms.fromBukkitPlayer(player.getName());
+        String playerTownName = "";
+        if (comPlayer.getCommunity() != null) {
+            playerTownName = comPlayer.getCommunity().getName();
+        }
+        Community chunkTown = coms.getChunkOwner(location.getChunk());
         String chunkTownName = "";
-				if(chunkTown != null) {
-					chunkTownName = chunkTown.getName();
-				}
-				return playerTownName.equalsIgnoreCase(chunkTownName);
+        if (chunkTown != null) {
+            chunkTownName = chunkTown.getName();
+        }
+        return playerTownName.equalsIgnoreCase(chunkTownName);
     }
 }
